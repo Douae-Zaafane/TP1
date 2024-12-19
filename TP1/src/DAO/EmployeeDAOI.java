43be46e6888ab34bd631ba32695771da53f5c713
Douae-Zaafane <douaezfn9@gmail.com>
2024-12-19 21:32:29 +0100

    Add files via upload

---
+++ TP1/src/DAO/EmployeeDAOI.java
@@ -0,0 +1,12 @@
+package DAO;
+
+import Model.Employee;
+import java.util.List;
+
+public interface EmployeeDAOI {
+    void add(Employee employee); 
+    void delete(int id);
+    List<Employee> listAll(); 
+    Employee findById(int id);
+    void update(Employee employee, int id); 
+}
